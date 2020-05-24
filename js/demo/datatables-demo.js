7f3af61f9bb720463a8a4f3a33bd84246a5d0140
IM <imran.meman@gmail.com>
2020-05-24 10:28:48 +0100

    enter

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -2,3 +2,26 @@
 $(document).ready(function () {
   $("#dataTable").DataTable();
 });
+
+/*let items = [];
+
+function addItem() {
+  let Item = {
+    name: document.getElementById(inputIt).value,
+    amount: document.getElementById(inpAmt).value,
+    date: document.getElementById(inpDate).value,
+    type: document.getElementById(itemttype).value,
+  };
+
+  items.push(Item);
+}*/
+
+$("#enterItem").show();
+$("#enterAmount").show();
+
+$("#enterAmount").keypress(function (e) {
+  var mylist = [$("#enterItem").val(), $("#enterAmount").val()];
+  if (e.which == 13) {
+    $("#list").prepend("<li>" + mylist + "</li>");
+  }
+});
